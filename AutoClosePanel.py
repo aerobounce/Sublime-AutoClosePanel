67dd4cfee68e9cdfb075a0d8b6e7bb3b1ca76b1e
aerobounce <10491362+aerobounce@users.noreply.github.com>
2022-01-30 15:00:51 +0900

    Format

--- AutoClosePanel.py
+++ AutoClosePanel.py
@@ -16,15 +16,18 @@ SETTINGS_FILENAME = "AutoClosePanel.sublime-settings"
 SETTINGS_KEYS = ["close_panel_on_save", "target_panels"]
 ON_CHANGE_TAG = "reload_settings"
 
+
 def plugin_loaded():
     AutoClosePanel.settings = sublime.load_settings(SETTINGS_FILENAME)
     AutoClosePanel.reload_settings()
     AutoClosePanel.settings.add_on_change(ON_CHANGE_TAG, AutoClosePanel.reload_settings)
 
+
 def plugin_unloaded():
     AutoClosePanel.settings.clear_on_change(ON_CHANGE_TAG)
 
-class AutoClosePanel():
+
+class AutoClosePanel:
     settings = sublime.load_settings(SETTINGS_FILENAME)
     is_plugin_enabled = False
     target_panels = {}
@@ -45,26 +48,32 @@ class AutoClosePanel():
     def hide_panel(cls, window):
         panel_name = (window.active_panel() or "").replace("output.", "")
 
-        if not panel_name in cls.target_panels.keys(): return
+        if not panel_name in cls.target_panels.keys():
+            return
         active_panel = window.find_output_panel(panel_name)
 
-        if active_panel == None: return
+        if active_panel == None:
+            return
         all_text = active_panel.substr(sublime.Region(0, active_panel.size()))
 
         for pattern in cls.target_panels[panel_name]:
             if re.match(pattern, all_text):
-                window.run_command("hide_panel", { "panel": "output." + panel_name })
+                window.run_command("hide_panel", {"panel": "output." + panel_name})
                 return
+
 
 class AutoClosePanelCloseCommand(sublime_plugin.WindowCommand):
     def run(self):
         AutoClosePanel.hide_panel(self.window)
 
+
 class AutoClosePanelPrintPanelsCommand(sublime_plugin.WindowCommand):
     def run(self):
         AutoClosePanel.print_all_window_panels()
 
+
 class AutoClosePanelListener(sublime_plugin.EventListener):
     def on_pre_save_async(self, view):
-        if not AutoClosePanel.is_plugin_enabled: return
+        if not AutoClosePanel.is_plugin_enabled:
+            return
         AutoClosePanel.hide_panel(view.window())
